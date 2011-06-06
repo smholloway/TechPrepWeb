03560e332b0f06d61b38a1c04c158826945be9f4
Seth Holloway <smholloway@gmail.com>
2011-06-06 15:25:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InterviewFlashcards::Application.config.secret_token = 'e43acc562965e3415542cb273a8aa51c9b56bccb5dbf7d8776d704a41ffc6c73c6a2316e527feae465f139c1a29a6da9d23ea93d36b3c19322dd6a38e33bfa38'
